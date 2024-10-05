90f469c3d8b365454795497fcbe895489a32e716
yanos1 <yanos241999@gmail.com>
2024-10-05 18:47:38 +0300

    bug fix

--- Assets/Scripts/GameLogic/ObstacleGeneration/ChasingObstacleComponent.cs
+++ Assets/Scripts/GameLogic/ObstacleGeneration/ChasingObstacleComponent.cs
@@ -1,21 +1,27 @@
-﻿using Core.Managers;
+﻿using System;
+using Core.Managers;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace GameLogic.ObstacleGeneration
 {
     public class ChasingObstacleComponent : ObstacleComponent
     {
         private float minDistanceToMove = 0.15f;
+        private float speed;
+
+        private void Start()
+        {
+            speed = CoreManager.instance.ControlPanelManager.chasingObstaclesMovespeed;
+        }
 
         private void Update()
         {
-            print("CALLED UPDATEE");
             if (Mathf.Abs(transform.position.x - CoreManager.instance.Player.transform.position.x) > minDistanceToMove)
             {
+                int direction = transform.position.x > CoreManager.instance.Player.transform.position.x ? 1 : -1;
 
-                transform.position += Vector3.left *
-                                      (CoreManager.instance.ControlPanelManager.GetObstacleSpeedMuliplier(CoreManager
-                                           .instance.ControlPanelManager.chasingObstaclesMovespeed) * Time.deltaTime);
+                transform.position += Vector3.left * (CoreManager.instance.ControlPanelManager.GetObstacleSpeedMuliplier(speed) * Time.deltaTime * direction);
             }
         }
     }
